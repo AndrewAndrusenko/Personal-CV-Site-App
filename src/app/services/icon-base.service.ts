d710b5e2224fcde6febcd13f9b33b6335db7761e
Andrey Andrusenko <aandrusenko@yandex.ru>
2026-05-08 23:22:53 +0300

    Icons purge

--- src/app/services/icon-base.service.ts
+++ src/app/services/icon-base.service.ts
@@ -14,59 +14,12 @@ export class IconBaseService {
         'account-pin-circle-line',
         'exchange-box-fill',
         'lock-password-fill'
-    /* 'add-box-fill',
-    'add-box-line',
-    'arrow-down-fill',
-    'arrow-down-s-line',
-    'arrow-up-s-line',
-    'add-fill',
-    'arrow-left-line',
-    'ball-pen-fill',
-    'bank-fill',
-    'book-marked-fill',
-    'building-4-fill',
-    'calendar-event-fill',
-    'checkbox-fill',
-    'checkbox-multiple-fill',
-    'close-line',
-    'contract-line',
-    'cup-fill',
-    'delete-bin-5-line',
-    'delete-bin-6-line',
-    'delete-bin-fill',
-    'download-2-line',
-    'edit-line',
-    'edit-2-fill',
-    'eye-line',
-    'file-copy-2-fill',
-    'file-copy-line',
-    'file-edit-fill',
-    'file-text-fill',
-    'first-aid-kit-fill',
-    'folder-line',
-    'home-4-fill',
-    'information-fill',
-    'information-line',
-    'links-fill',
-    'map-pin-range-fill',
-    'pie-chart-2-fill',
-    'save-2-fill',
-    'search-eye-fill',
-    'settings-3-fill',
-    'settings-4-fill',
-    'shopping-cart-fill',
-    'timer-2-line',
-    'timer-line',
-    'upload-2-line',
-    'user-3-fill',
-    'user-settings-fill',
-    'vip-crown-2-fill', */
-])
-  constructor () {
-    const iconRegistry = inject(MatIconRegistry);
-    const sanitizer = inject(DomSanitizer);
-    this.iconsStore.forEach(icon=>{
-      iconRegistry.addSvgIcon(icon, sanitizer.bypassSecurityTrustResourceUrl('assets/icons/'+icon+'.svg'));
-    })
+    ])
+    constructor () {
+        const iconRegistry = inject(MatIconRegistry);
+        const sanitizer = inject(DomSanitizer);
+        this.iconsStore.forEach(icon=>{
+            iconRegistry.addSvgIcon(icon, sanitizer.bypassSecurityTrustResourceUrl('assets/icons/'+icon+'.svg'));
+        })
   }
 }
