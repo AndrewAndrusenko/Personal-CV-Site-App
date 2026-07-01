a1c0810cb43f9ac4242ac7f1f8b6bb1b513102dc
Andrey Andrusenko <aandrusenko@yandex.ru>
2026-07-01 17:52:04 +0300

    Projects routes via settings json Projects URLs has been moved to routes.json file. Created service to read them flexibly and async pipe to use them in the template

--- src/app/components/aam-core-info/aam-core-info.component.ts
+++ src/app/components/aam-core-info/aam-core-info.component.ts
@@ -1,12 +1,20 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
+import { Observable } from 'rxjs';
 @Component({
     selector: 'app-aam-core-info',
     templateUrl: './aam-core-info.component.html',
     styleUrls: ['./aam-core-info.component.scss'],
-    imports:[NgOptimizedImage]
+    imports:[
+        NgOptimizedImage,
+        AsyncPipe
+    ]
 })
 export class AamCoreInfoComponent {
-
+    projectUrl$!: Observable<string>
+    settingService = inject(SettingsService)
+    ngOnInit(): void {
+        this.projectUrl$ = this.settingService.getSettings('projects.aam.url')
+    }
 }
